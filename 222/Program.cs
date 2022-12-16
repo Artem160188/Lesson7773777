4912a11feeadb2b47a9d27a3a9b152b6836e36d3
Artem160188 <118046594+Artem160188@users.noreply.github.com>
2022-12-16 23:34:28 +0300

    222

--- 222/Program.cs
+++ 222/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 50. Напишите программу, которая на вход принимает 
+// значение элемента в двумерном массиве, и возвращает позицию 
+// этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+int[,] a1 = { { 1, 2,  }, { 2, 3 }, { 3, 4 } };
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+bool check = false;
+for (int i = 0; i < a1.GetLength(0); i++)
+{
+    for (int j = 0; j < a1.GetLength(1); j++)
+    {
+        if (num == a1[i, j])
+        {
+            check = true;
+            Console.WriteLine($"Элемент имеет позицию {i} {j}");
+        }
+    }
+}
+if (check == false)
+{
+    Console.WriteLine("Такого элемента нет");
+}
+
